83a2e0617fc8082c96afdae760552329b23f9651
yntymakdev <kubanychevyntymak21@gmail.com>
2025-01-20 21:32:46 +0600

    added created feature

--- src/services/color.service.ts
+++ src/services/color.service.ts
@@ -1,6 +1,6 @@
 import { axiosWithAuth } from "@/api/api.interceptors";
 import { API_URL } from "@/config/api.config";
-import { IColor } from "@/shared/types/color.interface";
+import { IColor, IColorInput } from "@/shared/types/color.interface";
 
 class ColorService {
   async getByStoreId(id: string) {
@@ -18,5 +18,14 @@ class ColorService {
     });
 
     return data;
+  }
+  async create(data: IColorInput, storeId: string) {
+    const { data: createdColor } = await axiosWithAuth<IColor>({
+      url: API_URL.colors(`/${storeId}`),
+      method: "POST",
+      data,
+    });
+
+    return createdColor;
   }
 }
