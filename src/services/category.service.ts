6fefe0f1a3e14cc59e4e06fcbc2d5cd94c1704d6
yntymakdev <kubanychevyntymak21@gmail.com>
2025-01-20 20:29:33 +0600

    added getbyId function feature for service category

--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -1,4 +1,4 @@
-import { axiosWithAuth } from "@/api/api.interceptors";
+import { axiosClassic, axiosWithAuth } from "@/api/api.interceptors";
 import { API_URL } from "@/config/api.config";
 import { ICategory } from "@/shared/types/category.interface";
 
@@ -6,6 +6,15 @@ class CategoryService {
   async getByStoreId(id: string) {
     const { data } = await axiosWithAuth<ICategory[]>({
       url: API_URL.categories(`/by-storeId/${id}`),
+      method: "GET",
+    });
+
+    return data;
+  }
+
+  async getById(id: string) {
+    const { data } = await axiosClassic<ICategory>({
+      url: API_URL.categories(`/by-id/${id}`),
       method: "GET",
     });
 
