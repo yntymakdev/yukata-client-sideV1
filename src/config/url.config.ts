66ed7bd930618062405673310a2e565e9287b1a4
yntymakdev <kubanychevyntymak21@gmail.com>
2025-01-20 15:46:59 +0600

    url config costumized

--- src/config/url.config.ts
+++ src/config/url.config.ts
@@ -1,5 +1,11 @@
 export const APP_URL = process.env.APP_URL as string;
 
-export const PUBLIC_URL = {};
+export const PUBLIC_URL = {
+  root: (url = "") => `${url ? url : ""}`,
+  home: () => PUBLIC_URL.root("/auth"),
+  explorer: (query = "") => PUBLIC_URL.root(`/explorer${query}`),
+  product: (id = "") => PUBLIC_URL.root(`/product/${id}`),
+  explorer: (query = "") => PUBLIC_URL.root(`/explorer${query}`),
+};
 export const DASHBOARD_URL = {};
 export const STORE_URL = {};
