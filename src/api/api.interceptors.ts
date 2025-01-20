17b3285f46388bc7624c8f271a1b2986b47c97d3
yntymakdev <kubanychevyntymak21@gmail.com>
2025-01-20 20:21:57 +0600

    added auth service

--- src/api/api.interceptors.ts
+++ src/api/api.interceptors.ts
@@ -1,8 +1,49 @@
-import { CreateAxiosDefaults } from "axios";
-import { getContentType } from "./api.helpers";
+import axios, { CreateAxiosDefaults } from "axios";
+
 import { SERVER_URL } from "@/config/api.config";
+import { errorCatch, getContentType } from "./api.helpers";
+import { getAccessToken, removeFromStorage } from "@/services/auth/auth-tokenservice";
+
 const options: CreateAxiosDefaults = {
   baseURL: SERVER_URL,
   headers: getContentType(),
   withCredentials: true,
 };
+
+const axiosClassic = axios.create(options);
+const axiosWithAuth = axios.create(options);
+
+axiosWithAuth.interceptors.request.use((config) => {
+  const accessToken = getAccessToken();
+
+  if (config?.headers && accessToken) config.headers.Authorization = `Bearer ${accessToken}`;
+
+  return config;
+});
+
+axiosWithAuth.interceptors.response.use(
+  (config) => config,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (
+      (error?.response?.status === 401 ||
+        errorCatch(error) === "jwt expried" ||
+        errorCatch(error) === "jwt must be provided") &&
+      error.config &&
+      !error.config._isRetry
+    ) {
+      originalRequest._isRetry = true;
+      try {
+        await auth.getNewTokens();
+        return axiosWithAuth.request(originalRequest);
+      } catch (error) {
+        if (errorCatch(error) === "jwt expired") removeFromStorage();
+      }
+    }
+
+    throw error;
+  }
+);
+
+export { axiosClassic, axiosWithAuth };
